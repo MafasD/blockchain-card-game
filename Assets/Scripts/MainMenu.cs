2242145395ad1335a85635de52115ff4cf9f4b04
Hua Chen <k8chhu00@students.oamk.fi>
2024-04-24 21:53:18 +0300

    edit StartGame button link to GameScene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("GameScene");
     }
 
     // Update is called once per frame
