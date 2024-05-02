53c888a9f780a1dea9494c6b009714f89ecb185e
Mafas Dhaimash <mafas@hotmail.fi>
2024-05-02 22:56:30 +0300

    Added 3 enemy decks method

--- Assets/Scripts/Controller Scripts/DeckController.cs
+++ Assets/Scripts/Controller Scripts/DeckController.cs
@@ -5,17 +5,23 @@ using UnityEngine.UI;
 
 public class DeckController : MonoBehaviour
 {
-    public DeckManager playerDeckManager; 
-    public DeckManager enemyDeckManager;
+    public DeckManager playerDeckManager;
+    public List<DeckManager> enemyDeckManager;
     public Transform playerHand;
     public Transform enemyHand;
     public Button drawButton;
+
+    private DeckManager chosenEnemyDeck;
 
     private void Start()
     {
         //Sets player's and enemy's hand references
         playerDeckManager.hand = playerHand;
-        enemyDeckManager.hand = enemyHand;
+        //Chooses a random enemy deck
+        int randomIndex = Random.Range(0, enemyDeckManager.Count);
+        // Set enemy's hand reference
+        chosenEnemyDeck = enemyDeckManager[randomIndex];
+        chosenEnemyDeck.hand = enemyHand;
         //Checks the initial state of the draw button
         CheckDrawButtonState();
     }
@@ -23,17 +29,16 @@ public class DeckController : MonoBehaviour
     public void DrawCards()
     {
         playerDeckManager.DrawCard(5); //Draws 5 cards for the player
-        enemyDeckManager.DrawCard(5); //Draws 5 cards for the enemy
+        chosenEnemyDeck.DrawCard(5); //Draws 5 cards from the randomly chosen enemy deck
         //Checks the state of the draw button after drawing cards
         CheckDrawButtonState();
     }
     //Method to check the state of the draw button
     public void CheckDrawButtonState()
     {
-        //Checks if decks are not empty and that hands have at least one card or less (It's actually 0, but the counting does not work properly for some reason).
-        bool playerDeckNotEmpty = playerDeckManager.deck.Count > 0 && playerHand.childCount <= 1;
-        bool enemyDeckNotEmpty = enemyDeckManager.deck.Count > 0 && enemyHand.childCount <= 1;
-        //If all conditions are met, draw button becomes interactable.
-        drawButton.interactable = playerDeckNotEmpty && enemyDeckNotEmpty;
+        //Checks if deck is not empty and if hand is empty
+        bool playerDeckNotEmpty = playerDeckManager.deck.Count > 0 && playerHand.childCount == 0;
+        //If all conditions are met, draw button becomes interactable
+        drawButton.interactable = playerDeckNotEmpty;
     }
 }
