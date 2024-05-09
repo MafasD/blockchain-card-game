168295fd3b0d65da48b1da15780279e2934ec19a
Hua Chen <k8chhu00@students.oamk.fi>
2024-05-09 16:15:14 +0300

    ui updated

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject settingsParent;
+    private bool settingsOn = false;
+
+    public void OnClickToggleSettings()
+    {
+        settingsOn = !settingsOn;
+        settingsParent.SetActive(settingsOn);
+    }
     // Start is called before the first frame update
     public void StartGame()
     {
